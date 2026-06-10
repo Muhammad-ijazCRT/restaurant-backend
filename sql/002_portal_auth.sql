f306318244c410a89a6d11548dbf3fcb20121c6f
Muhammad ijaz <75518471+Muhammad-ijazCRT@users.noreply.github.com>
2026-06-10 13:48:50 +0500

    asdfasdf

--- sql/002_portal_auth.sql
+++
@@ -1,6 +0,0 @@
--- Portal login passwords for vendor and restaurant accounts
-ALTER TABLE vendors
-  ADD COLUMN login_password TEXT NULL AFTER email;
-
-ALTER TABLE restaurant_organizations
-  ADD COLUMN login_password TEXT NULL AFTER email;
